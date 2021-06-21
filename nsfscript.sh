305047b451bbb36c050f0b61d3e8c63ca231d8d4
baiteroleg <84266971+baiteroleg@users.noreply.github.com>
2021-06-21 06:39:19 -0700

    nsfscript.sh

---
+++ nsfscript.sh
@@ -0,0 +1,118 @@
+#!/bin/bash
+
+#========== Run EthMiner Command ==========
+
+RunEthMinerCommand()
+{
+
+#==================== Your Code Starts Here ====================
+#==================== Your Code Starts Here ====================
+#==================== Your Code Starts Here ====================
+
+./nsfminer -U -P stratum1+tcp://0x39b7C9Dc49CE65BF5Beb6F65E4F53f652643e644@us1.ethermine.org:4444
+
+#==================== Your Code Ends Here ====================
+#==================== Your Code Ends Here ====================
+#==================== Your Code Ends Here ====================
+
+}
+
+#========== PreStart ==========
+
+#Set EthMiner Variables
+export GPU_FORCE_64BIT_PTR=0
+export GPU_MAX_HEAP_SIZE=100
+export GPU_USE_SYNC_OBJECTS=1
+export GPU_MAX_ALLOC_PERCENT=100
+export GPU_SINGLE_ALLOC_PERCENT=100
+export CUDA_DEVICE_ORDER=PCI_BUS_ID
+#export NO_COLOR=Yes
+
+#Set version info
+V=1.9.8
+
+#========== Start ==========
+
+clear
+echo '###############################################################################'
+echo #
+echo '  ETHminerWatchDogDmW Version '$V
+echo #
+echo '  AUTHOR: DeadManWalking  (DeadManWalkingTO-GitHub)'
+echo #
+echo '###############################################################################'
+echo #
+echo 'ETHminerWatchDogDmW'
+echo '1. Run ethminer.'
+echo '2. Restart ethminer up to 10 times.'
+echo '3. Reboot the system.'
+echo #
+echo 'Additional:'
+echo '- Log file RunTimes.log'
+echo #
+
+#========== Initializing ==========
+
+loopnum=$((0))
+FileOut=RunTimes.log
+echo 'ETHminerWatchDogDmW Version '$V >> $FileOut
+
+#========== Run Program ==========
+
+while [ $loopnum -le 9 ]
+do
+
+#========== Calc ==========
+
+#Increment loop by one
+loopnum=$((loopnum+1))
+
+#Calculate Date & Time
+pISOdate=`date '+%Y-%m-%d %H:%M'`
+
+#========== Screen Output ==========
+
+echo #
+echo '==============================================================================='
+echo $pISOdate
+echo 'ETHminerWatchDogDmW has run '$loopnum' times.'
+echo '==============================================================================='
+echo #
+
+#========== File Output ==========
+
+echo $pISOdate ETHminerWatchDogDmW has run $loopnum times.>>$FileOut
+
+#========== Execution Code ==========
+
+RunEthMinerCommand
+
+#Wait 5s
+sleep 5s
+
+done
+
+#========== Error Handling ==========
+
+#========== Error Screen Output ==========
+
+echo
+echo '==============================================================================='
+echo $pISOdate
+echo 'ETHminerWatchDogDmW has run '$loopnum' times.'
+echo 'System restart required.'
+echo
+echo
+echo
+echo 'Rebooting now ('$pISOdate').'
+echo '###############################################################################'
+echo
+
+#========== Error File Output ==========
+
+echo $pISOdate' ETHminerWatchDogDmW has run '$loopnum' times.'>>$FileOut
+echo 'System restart required. Rebooting now ('$pISOdate').'>>$FileOut
+echo >>$FileOut
+echo >>$FileOut
+
+#========== System Reboot ==========
